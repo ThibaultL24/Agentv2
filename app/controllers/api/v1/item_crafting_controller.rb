072ab3bff9055f0066da04bd1cc20b9d5ce33a1e
ThibaultL24 <thibault.lenormand24@gmail.com>
2025-01-14 14:40:38 +0100

    correction on item_crafting_controller

--- app/controllers/api/v1/item_crafting_controller.rb
+++ app/controllers/api/v1/item_crafting_controller.rb
@@ -16,9 +16,14 @@ class Api::V1::ItemCraftingController < ApplicationController
 
   def calculate_crafting_metrics
     {
-      crafting_cost: @item_crafting.nbLowerBadgeToCraft * base_badge_cost,
+      crafting_cost: calculate_total_crafting_cost,
       units_per_craft: @item_crafting.unitToCraft,
       efficiency_ratio: @item_crafting.unitToCraft / @item_crafting.nbLowerBadgeToCraft
     }
+  end
+
+  def calculate_total_crafting_cost
+    base_cost = @item_crafting.nbLowerBadgeToCraft * @item_crafting.item.base_badge_cost
+    base_cost + @item_crafting.additional_costs
   end
 end
